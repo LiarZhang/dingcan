112256962b76b008827f51e1f0a60e0e1ec7fa4b
zhangxiaofeng <2532466427@qq.com>
2018-01-08 12:04:40 +0800

    定时任务

---
+++ src/main/java/com/zhangxf/dingcan/task/ScheduledTask.java
@@ -0,0 +1,21 @@
+package com.zhangxf.dingcan.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/*
+ * @date: 2018年1月8日
+ * @author: Zhangxf
+ * @version :1.0.0
+*/
+@Component
+public class ScheduledTask {
+	@Scheduled(fixedRate = 3000)
+	public void helloTask(){
+		String date=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+		System.out.println("时间:"+date);
+	}
+}
